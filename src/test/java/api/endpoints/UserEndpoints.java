044cf83a95668e8cfd8e0513ebdd6fa66ef49b5e
akshatakanaje <111020296+akshatakanaje@users.noreply.github.com>
2025-04-15 22:30:27 -0700

    added test packages

---
+++ src/test/java/api/endpoints/UserEndpoints.java
@@ -0,0 +1,12 @@
+package api.endpoints;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class UserEndpoints {
+
+
+
+}
