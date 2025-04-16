2e9f62ef927736855fab026ca702a0c320114a3a
akshatakanaje <111020296+akshatakanaje@users.noreply.github.com>
2025-04-15 22:43:33 -0700

    added user class and user endpoints

--- src/test/java/api/payload/User.java
+++ src/test/java/api/payload/User.java
@@ -1,5 +1,19 @@
 package api.payload;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 
+    int id;
+    String userName;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String phone;
+    int userStatus=0;
 }
